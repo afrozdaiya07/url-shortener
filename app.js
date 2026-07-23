e7f6fb92fa165d07584a36a64427ddfb00f4f796
Afroz daiya07 <afrozdaiya07@gmail.com>
2026-07-23 11:23:44 +0530

    change code

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 7001;
 
 // MongoDB Connection
 connectToMongoDB(process.env.MONGO_URL)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.log("❌ MongoDB Error:", err));
+  .then(() => console.log(" MongoDB Connected"))
+  .catch((err) => console.log(" MongoDB Error:", err));
 
 // Middleware
 app.use(express.json());
@@ -25,10 +25,10 @@ app.set("views", path.join(__dirname, "views"));
 
 // Home Route
 app.get("/", (req, res) => {
-  res.send("🚀 Short URL Project Started");
+  res.send("Short URL Project Started");
 });
 
 // Server
 app.listen(PORT, () => {
-  console.log(`✅ Server Running on http://localhost:${PORT}`);
+  console.log(` Server Running on http://localhost:${PORT}`);
 });
